1ee304b78948978f55d5253c6751bea4b33a7a07
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-02-18 12:26:52 +0800

    My Personal Info added

---
+++ git-python-Soleta.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 12:19:43 2024
+
+@author: Ira Kristine S. Soleta
+"""
+
+print ("Full Name: Ira Kristine S. Soleta")
+print ("Address: Bagacay, Buenavista, Marinduque")
+print ("Birthday: 07-01-2023")
